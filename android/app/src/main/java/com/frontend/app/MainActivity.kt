add53a708a8e2b110efaf498e61056408c63207e
Sathish728 <sathishdsds0728@gmail.com>
2025-12-05 13:38:22 +0530

    third commit

---
+++ android/app/src/main/java/com/frontend/app/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.frontend;  // CHANGE THIS
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
+
+public class MainActivity extends ReactActivity {
+  @Override
+  protected String getMainComponentName() {
+    return "frontend";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new DefaultReactActivityDelegate(
+        this,
+        getMainComponentName(),
+        DefaultNewArchitectureEntryPoint.getFabricEnabled());
+  }
+}
